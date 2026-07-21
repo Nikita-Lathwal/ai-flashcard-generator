cb2b728fccece5fcd453261821351a76d2f92dc8
Nikita Lathwal <08a.nikitalathwal.lmia@gmail.com>
2026-07-21 16:45:08 +0530

    Complete Milestone 1: Backend setup and MongoDB Atlas integration

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
 
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Backend is running successfully!");
+  res.send("FlashMind AI Backend is Running 🚀");
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
