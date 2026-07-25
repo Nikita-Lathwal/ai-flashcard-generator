64acf84ccdd5a25e7a191b9b3b47abdca36bed23
nisha.meena <nishagunawat1977@gmail.com>
2026-07-25 15:25:17 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("userInfo"));
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar">
       <h2 className="logo">
@@ -11,11 +20,23 @@ const Navbar = () => {
         <a href="#features">Features</a>
         <a href="#about">About</a>
 
-        <Link to="/login">Login</Link>
+        {user ? (
+          <>
+            <Link to="/dashboard">Dashboard</Link>
 
-        <Link to="/signup" className="signup-btn">
-          Signup
-        </Link>
+            <button onClick={logoutHandler}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login">Login</Link>
+
+            <Link to="/signup" className="signup-btn">
+              Signup
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
