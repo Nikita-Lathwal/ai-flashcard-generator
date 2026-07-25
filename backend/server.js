0faf2932e94b347cfd749dde49b7b20a4c4d68f4
nisha.meena <nishagunawat1977@gmail.com>
2026-07-25 15:24:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -19,6 +25,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("FlashMind AI Backend is Running 🚀");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
